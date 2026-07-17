a75a4ce78f4c23abc0df680d7ea71b0d90c6edb1
Creativeroboboy <mohammedismael000@gmail.com>
2026-07-17 18:25:29 +0300

    Remove all files except Face Mesh.py

--- FaceMesh.py
+++
@@ -1,6 +0,0 @@
-import runpy
-from pathlib import Path
-
-if __name__ == "__main__":
-    script_path = Path(__file__).with_name("Face Mesh.py")
-    runpy.run_path(str(script_path), run_name="__main__")
